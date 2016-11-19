dfe205a6f193f81ab41f3ceeafcbfbdc0262398b
LeTai <vzttzv@gmail.com>
2016-11-19 20:05:44 +0700

    .

--- dev/js/ga.js
+++ dev/js/ga.js
@@ -45,19 +45,3 @@ window.addEventListener('beforeinstallprompt', function(e) {
     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1794039710810680";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-// window.twttr = (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0],
-//         t = window.twttr || {};
-//     if (d.getElementById(id)) return t;
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://platform.twitter.com/widgets.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-
-//     t._e = [];
-//     t.ready = function(f) {
-//         t._e.push(f);
-//     };
-
-//     return t;
-// }(document, "script", "twitter-wjs"));
