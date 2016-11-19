8578393a612947ed295e096051900cece69423a4
LeTai <vzttzv@gmail.com>
2016-11-19 14:48:16 +0700

    offline capability

--- docs/sw.js
+++ docs/sw.js
@@ -1,27 +1,40 @@
-self.addEventListener('install', function(event) {
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = [
-        '/',
-        '/styles.min.css',
-        '/scripts.min.js'
-    ];
+ var cacheName = 'pwasleeptime';
+ var urlsToCache = [
+     '/',
+     '/index.html',
+     '/styles.min.css',
+     '/scripts.min.js'
+ ];
 
-    self.addEventListener('install', function(event) {
-        // Perform install steps
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-        );
-    });
-});
-self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
-});
+ self.addEventListener('install', function(event) {
+     // Perform install steps
+     event.waitUntil(
+         caches.open(cacheName)
+         .then(function(cache) {
+             console.log('Opened cache');
+             return cache.addAll(urlsToCache);
+         })
+     );
+ });
+ self.addEventListener('activate', function(e) {
+     console.log('[ServiceWorker] Activate');
+     e.waitUntil(
+         caches.keys().then(function(keyList) {
+             return Promise.all(keyList.map(function(key) {
+                 if (key !== cacheName) {
+                     console.log('[ServiceWorker] Removing old cache', key);
+                     return caches.delete(key);
+                 }
+             }));
+         })
+     );
+     return self.clients.claim();
+ });
+ self.addEventListener('fetch', function(e) {
+     console.log('[ServiceWorker] Fetch', e.request.url);
+     e.respondWith(
+         caches.match(e.request).then(function(response) {
+             return response || fetch(e.request);
+         })
+     );
+ });
