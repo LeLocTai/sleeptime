ea6421f09d3353a6538957cd3d1f8041688710a3
LeTai <vzttzv@gmail.com>
2017-05-03 17:34:34 +0700

    redesign

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
- var cacheName = 'pwasleeptime-v1.0.1';
+ var cacheName = 'pwasleeptime-v1.2';
  var urlsToCache = [
      '',
      'index.html',
@@ -6,22 +6,28 @@
      'scripts.min.js'
  ];
 
- self.addEventListener('install', function(event) {
+ self.addEventListener('install', function (event)
+ {
      // Perform install steps
      event.waitUntil(
          caches.open(cacheName)
-         .then(function(cache) {
+         .then(function (cache)
+         {
              console.log('Opened cache');
              return cache.addAll(urlsToCache);
          })
      );
  });
- self.addEventListener('activate', function(e) {
+ self.addEventListener('activate', function (e)
+ {
      console.log('[ServiceWorker] Activate');
      e.waitUntil(
-         caches.keys().then(function(keyList) {
-             return Promise.all(keyList.map(function(key) {
-                 if (key !== cacheName) {
+         caches.keys().then(function (keyList)
+         {
+             return Promise.all(keyList.map(function (key)
+             {
+                 if (key !== cacheName)
+                 {
                      console.log('[ServiceWorker] Removing old cache', key);
                      return caches.delete(key);
                  }
@@ -30,10 +36,12 @@
      );
      return self.clients.claim();
  });
- self.addEventListener('fetch', function(e) {
+ self.addEventListener('fetch', function (e)
+ {
      console.log('[ServiceWorker] Fetch', e.request.url);
      e.respondWith(
-         caches.match(e.request).then(function(response) {
+         caches.match(e.request).then(function (response)
+         {
              return response || fetch(e.request);
          })
      );
